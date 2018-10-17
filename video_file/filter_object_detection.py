86f0022dd7cf4694f3360d99a8592aacb293dbdb
Guilherme Machado <guivmachado@gmail.com>
2018-10-17 10:16:41 -0300

    First Commit

---
+++ video_file/filter_object_detection.py
@@ -0,0 +1,113 @@
+# USAGE
+# python filter_object_detection.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import imutils
+import time
+import cv2
+import face_recognition
+from datetime import datetime
+# initialize the list of class labels MobileNet SSD was trained to
+# detect, then generate a set of bounding box colors for each class
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+    "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+    "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+    "sofa", "train", "tvmonitor"]
+
+my_image = face_recognition.load_image_file("me.jpg")
+my_face_encoding = face_recognition.face_encodings(my_image)[0]
+
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+IGNORE = set(["background", "aeroplane", "bicycle", "bird", "boat",
+    "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+    "horse", "motorbike","pottedplant", "sheep",
+    "sofa", "train", "tvmonitor"])
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe("MobileNetSSD_deploy.prototxt.txt", "MobileNetSSD_deploy.caffemodel")
+
+# initialize the video stream, allow the cammera sensor to warmup,
+# and initialize the FPS counter
+print("[INFO] starting video stream...")
+input_movie = cv2.VideoCapture("eu.mp4")
+length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
+frame_number = 0
+person_verifier = True
+minute = 0
+
+# loop over the frames from the video stream
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    ret, frame = input_movie.read()
+    frame_number += 1
+
+    # grab the frame dimensions and convert it to a blob
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+        0.007843, (300, 300), 127.5)
+
+    # pass the blob through the network and obtain the detections and
+    # predictions
+    net.setInput(blob)
+    detections = net.forward()
+
+    # loop over the detections
+    for i in np.arange(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the prediction
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the `confidence` is
+        # greater than the minimum confidence
+        if confidence > 0.5:
+            # extract the index of the class label from the
+            # `detections`
+            idx = int(detections[0, 0, i, 1])
+
+            # if the predicted class label is in the set of classes
+            # we want to ignore then skip the detection
+            if CLASSES[idx] in IGNORE:
+                continue
+            elif confidence < 0.8:
+                continue
+            if minute != datetime.today().minute:
+                rgb_frame = frame[:, :, ::-1]
+                # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+                face_locations = face_recognition.face_locations(rgb_frame)
+                face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+                (top, right, bottom, left) = face_locations
+                for face_encoding in face_encodings:
+                    matches = face_recognition.compare_faces(my_face_encoding, [face_encoding])
+                    if matches:
+                        print("Hey Guilherme")
+                        minute = 0
+                        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # draw the prediction on the frame
+            label = "{}: {:.2f}%".format(CLASSES[idx],
+                confidence * 100)
+            cv2.rectangle(frame, (startX, startY), (endX, endY),
+                COLORS[idx], 2)
+            y = startY - 15 if startY - 15 > 15 else startY + 15
+            cv2.putText(frame, label, (startX, y),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
+
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
